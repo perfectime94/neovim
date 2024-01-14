e6d092562f1b12bb4b4f13adcbbbe3e232be0af7
perfectime94 <lineumartins001@gmail.com>
2024-01-14 22:36:10 +0100

    add LazyGit
    Require lazygit installed

---
+++ lua/perfectime94/plugins/git.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'plenary',
+    },
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = '[F]ind [F]iles' },
+    },
+  },
+}
