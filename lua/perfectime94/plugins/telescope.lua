178440687481eb7fdc638079c35b2c9dff07ab08
perfectime94 <lineumartins001@gmail.com>
2024-01-18 23:48:19 +0100

    Add more keys to telescope
    Like search for git files only

--- lua/perfectime94/plugins/telescope.lua
+++ lua/perfectime94/plugins/telescope.lua
@@ -8,7 +8,8 @@ return {
     },
     keys = {
       { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = '[F]ind [F]iles' },
-      { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = '[F]ind [G]rep' },
+      { '<leader>flg', '<cmd>Telescope live_grep<cr>', desc = '[F]ind [G]rep' },
+      { '<leader>fgf', '<cmd>Telescope git_files<cr>', desc = '[F]ind [G]it [F]iles' },
       { '<leader>fgs', '<cmd>Telescope grep_string<cr>', desc = '[F]ind [G]rep current [S]tring' },
       { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = '[F]ind [B]uffers' },
       { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = '[F]ind [H]elp tags' },
