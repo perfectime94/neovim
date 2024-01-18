f0358f9850aef9455845fba4ae16f35e270ab626
perfectime94 <lineumartins001@gmail.com>
2024-01-18 23:47:54 +0100

    Set bunch of keybindings

--- lua/perfectime94/settings.lua
+++ lua/perfectime94/settings.lua
@@ -5,3 +5,13 @@ g.mapleader = ' '
 g.maplocalleader = ' '
 
 k.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move text easily
+k.set("v", "J", ":m '>+1<CR>gv=gv")
+k.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Easy way to exit insert mode
+k.set("i", "<C-c>", "<Esc>")
+
+-- Format file
+k.set("n", "<leader>f", vim.lsp.buf.format)
