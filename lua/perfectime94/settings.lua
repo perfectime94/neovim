b87a65821bb73ed28e3e7ea6a985c8b1d53ef0cc
perfectime94 <lineumartins001@gmail.com>
2024-01-20 16:49:01 +0100

    Format code

--- lua/perfectime94/settings.lua
+++ lua/perfectime94/settings.lua
@@ -7,11 +7,11 @@ g.maplocalleader = ' '
 k.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Move text easily
-k.set("v", "J", ":m '>+1<CR>gv=gv")
-k.set("v", "K", ":m '<-2<CR>gv=gv")
+k.set('v', 'J', ':m \'>+1<CR>gv=gv')
+k.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
 -- Easy way to exit insert mode
-k.set("i", "<C-c>", "<Esc>")
+k.set('i', '<C-c>', '<Esc>')
 
 -- Format file
-k.set("n", "<leader>f", vim.lsp.buf.format)
+k.set('n', '<leader>f', vim.lsp.buf.format)
